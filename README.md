ad3f068cc90f3fbfca17431b989c1a9798fe4c02
Oussama Keddar <oussama.keddar@epitech.eu>
2018-02-18 10:45:48 +0100

    understanding how map, and reduce works, and forEachMethod using arrow functions

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Testing
 training, tests, learning, having fun, having troubles, and getting solutions...I edited this on my terminal. 
 
+# Purpose
+Just for learning js with some es6-7 tips
+
+
+
 
 
 
---
+++ app.js
@@ -0,0 +1,30 @@
+
+											// loop
+ let planes = ['airbus', 'boeing', 'atr'];
+
+ // for(var k = 0, max = b.length; k < max; k++){
+ // 	console.log( 'Position' + k + 'is for the company ' + b[k])
+ // }
+								//  forEach Method, arrowFunction
+
+// planes.forEach(
+// 	(plane, index) => 
+// 		// console.log( index + ' for' + plane)
+// 		console.log(`This position ${index} goes for the ${plane}`)
+// 	);
+
+
+
+									// map 
+// 	function hello(n) {
+// 		return n * 2;
+// 	}
+// a = [2,4,5].map(hello)
+	
+// let sum = a.reduce(function(pv, cv) { return pv + cv; }, 0);
+// console.log(sum)
+
+								// reduce 
+
+					
+
