6f63167bbabed29952f2e70eda27ecb0de88c230
Oussama Keddar <oussama.keddar@epitech.eu>
2018-05-27 15:18:47 +0200

    vuejsPractice

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-
-											// loop
- let planes = ['airbus', 'boeing', 'atr'];
+ // let planes = ['airbus', 'boeing', 'atr'];
 
  // for(var k = 0, max = b.length; k < max; k++){
  // 	console.log( 'Position' + k + 'is for the company ' + b[k])
@@ -20,11 +18,68 @@
 // 		return n * 2;
 // 	}
 // a = [2,4,5].map(hello)
+
+// class Article {
+
+// 	custructor(name) {
+// 		this.name = "Human";
+// 	}
+// 	get walk() {
+// 	 console.log('My name is ' + name)
+// 	}
+// }
+// let data = new Article('Lol')
+
+// console.log(data.walk)
 	
-// let sum = a.reduce(function(pv, cv) { return pv + cv; }, 0);
+// let sum = a.reduce((pv, cv) => { return pv + cv; }, 0);
 // console.log(sum)
 
 								// reduce 
 
-					
+ // var user = new Map();
+ // user.set(1, 'Oussama')
+ // user.set(2, 'Estelle');
+
+ // console.log(user.size);
+		// let hi = "Hello world";
+		// a = document.getElementById('root');	
+		// console.log(a)	
+// var psales = [ 100, 230, 55];
+// totalAfterTaxSales = psales.map( (amount) => amount * 1.5 ).reduce( (acc,val) => acc + val );
+
+//             
+// console.log(totalAfterTaxSales)
+
+// let str = String('Hey');
+// let newStr = new String ('Hello')
+// console.log(newStr
+// 	)
+// let text = "This is a list of items:cherries, limes, oranges, apples."
+
+// let start = text.indexOf(':');
+
+// let end = text.indexOf('.', start)
+
+// let fruits = text.substring(start +1, end)
+// let fruitList =fruits.split(',');
+// console.log(fruitList)
+
+// fruitList.forEach( function(element, index) {
+// 	console.log(element)
+// });
+
+function perc(score) {
+return score * 180 / 100;
+}
+console.log(perc(25))
+
+
+
+
+
+
+
+
+
 
